78baeb09a9f0611aa34db8426c51417e9605f832
leszko <rafal.leszko@gmail.com>
2017-02-22 07:11:57 +0100

    Check branch auto build

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main Spring Application. Test
+ * Main Spring Application. Test. Branch.
  */
 @SpringBootApplication
 public class CalculatorApplication {
